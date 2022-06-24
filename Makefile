6290c7c55b1e92f079f3513a787ed4f0847874a8
Gabriele Russo Russo <gabri.russo17@gmail.com>
2022-06-24 14:53:13 +0200

    Adds benchmark.yml

--- Makefile
+++ Makefile
@@ -4,4 +4,6 @@ all:
 	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yaml -u ec2-user $(ANS_FLAGS) site.yml
 check:
 	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i inventory.aws_ec2.yaml -u ec2-user $(ANS_FLAGS) check.yml
+benchmark:
+	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i inventory.aws_ec2.yaml -u ec2-user $(ANS_FLAGS) benchmark.yml
 
---
+++ examples/sieve.js
@@ -0,0 +1,25 @@
+// Example ported from TinyFaaS (https://github.com/OpenFogStack/tinyFaaS/blob/master/examples/sieve-of-erasthostenes/index.js)
+//
+module.exports = (params, ctx) => {
+	var max;
+
+	if (params["n"] == undefined) {
+		max = 1000
+	} else {
+		max = parseInt(params["n"],10);
+	}
+
+	let sieve = [], i, j, primes = [];
+	for (i = 2; i <= max; ++i) {
+
+		if (!sieve[i]) {
+			primes.push(i);
+			for (j = i << 1; j <= max; j += i) {
+				sieve[j] = true;
+			}
+		}
+	}
+
+	result = {"N": max, "Primes": primes.toString()}
+	return result
+}
