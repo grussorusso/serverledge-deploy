b1b7548a5e47c91da66dd383b42f2ed9f0fc23a0
Gabriele Russo Russo <gabri.russo17@gmail.com>
2023-08-22 12:43:09 +0200

    Minor changes

--- files/monitorCPU.sh
+++ files/monitorCPU.sh
@@ -6,5 +6,5 @@ while true; do
 #MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 CPU=$(uptime | awk '{printf "%.2f\t\t\n", $(NF-2)}')
 echo "$CPU" >> $outfile
-sleep 60
+sleep 30
 done
--- run_users_experiment.sh
+++ run_users_experiment.sh
@@ -2,17 +2,15 @@
 /home/gabriele/utils/fix_cpu_freq
 # XXX
 
-RESULTS_DIR="results_nero_cap3_noturbo_fixfreq"
+EXP_TAG="prova"
+RESULTS_DIR="results_${EXP_TAG}"
 
 [[ -d $RESULTS_DIR ]] || mkdir -p $RESULTS_DIR 
 
 ansible-playbook deploy_serverledge.yml
 
-#for users in $(seq 17 2 71); do
-for users in 1 5 10 15 20 25 30 40 50 60 70; do
-#for users in 60 80 100 120 140; do
-#for users in 140 150 170 180; do
-#for users in 200 220 240; do
+for users in 1 5; do
+	sleep 30
 	ansible-playbook -vv -e "users=${users}" \
 		-e "local_results_file=${RESULTS_DIR}/results-$users.csv"\
 		-e "local_responses_file=${RESULTS_DIR}/responses-$users.tar.gz"\
