0c50d7c9bf94094cfeb91c4ad5d056737fa0aa0f
Matteo Nardelli <matteo.nardelli@bancaditalia.it>
2026-01-02 11:54:38 +0100

    Update params of the sentimentAnalysis workflow

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -68,15 +68,15 @@ WORKFLOW_DATA.append(WorkflowData("weatherApp", "src/weather/weather.json", cmds
 #
 cmds = []
 
-cmds.append("create -u --function sa_retrieve --memory 256 --runtime custom --custom_image matnar/sa-retrieve --input data_url:Text --input local_dir:Text --input object_name:Text --output status:Text  --output local_download:Bool --output uploaded:Bool --output object_name:Text")
+cmds.append("create -u --function sa_retrieve --memory 256 --runtime custom --custom_image matnar/sa-retrieve --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input data_url:Text --input local_dir:Text --input object_name:Text --output status:Text  --output local_download:Bool --output uploaded:Bool --output object_name:Text")
 
 
-cmds.append("create -u --function sa_extract --memory 256 --runtime custom --custom_image matnar/sa-extract --input tgz_input_object_name:Text --input subset:Float --input local_dataset_file:Text --input local_output_dir:Text --input output_train_object_name:Text --input output_test_object_name:Text --output status:Text --output train_object_name:Text --output test_object_name:Text")
+cmds.append("create -u --function sa_extract --memory 256 --runtime custom --custom_image matnar/sa-extract --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input tgz_input_object_name:Text --input subset:Float --input local_dataset_file:Text --input local_output_dir:Text --input output_train_object_name:Text --input output_test_object_name:Text --output status:Text --output train_object_name:Text --output test_object_name:Text")
 
 
-cmds.append("create -u --function sa_train --memory 1224 --runtime custom --custom_image matnar/sa-train --input subset:Float --input max_features:Int --input train_object_data:Text --input local_train_file:Text --input local_model_file:Text --input local_vectorizer_file:Text --input output_model_object:Text --input output_vectorizer_object:Text --output status:Text --output model_object_name:Text --output vectorizer_object_name:Text")
+cmds.append("create -u --function sa_train --memory 1224 --runtime custom --custom_image matnar/sa-train --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input subset:Float --input max_features:Int --input train_object_data:Text --input local_train_file:Text --input local_model_file:Text --input local_vectorizer_file:Text --input output_model_object:Text --input output_vectorizer_object:Text --output status:Text --output model_object_name:Text --output vectorizer_object_name:Text")
 
-cmds.append("create -u --function sa_evaluate --memory 512 --runtime custom --custom_image matnar/sa-evaluate --input test_object_data:Text --input local_test_file:Text --input subset:Float --input local_model_file:Text --input local_vectorizer_file:Text --input input_model_object:Text --input input_vectorizer_object:Text --output status:Text --output accuracy:Float")
+cmds.append("create -u --function sa_evaluate --memory 512 --runtime custom --custom_image matnar/sa-evaluate --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input test_object_data:Text --input local_test_file:Text --input subset:Float --input local_model_file:Text --input local_vectorizer_file:Text --input input_model_object:Text --input input_vectorizer_object:Text --output status:Text --output accuracy:Float")
 
 WORKFLOW_DATA.append(WorkflowData("sentimentAnalysis", "src/sentiment/workflow.json", cmds, ["input/sentiment1.json"]))
 
