ef28ed7ec8c2954ac46d1602396b5f9131724cc1
Gabriele Russo Russo <gabri.russo17@gmail.com>
2026-01-05 10:53:30 +0100

    Fix workflow creation with Ansible

--- README.md
+++ README.md
@@ -1,61 +1,11 @@
-This repository contains scripts and utilities to automate the deployment of
-[Serverledge](https://github.com/grussorusso/serverledge).
 
-The repository mostly consists of Ansible playbooks and roles, which can be
-used to deploy Serverledge over existing hosts.
-In addition, we also provide Terraform scripts in the `terraform` directory,
-which can be used to provision a set of Amazon EC2 instances.
-
-Currently, we only support deployment to Debian-based Linux distributions
-or to Amazon Linux 2.
-
-**Important note**: while sensible defaults are used for most the variables,
-you should check and customize the scripts we provide before using them.
-
-## Requirements
-
-Install the following Ansible role, which is used to configure Docker:
-
-	ansible-galaxy install geerlingguy.docker
-
-
-## Ansible
-
-### Inventory
-
-Create an Ansible inventory file (default path is `inventory/default.ini`).
-Group the hosts using the following groups:
-
-- `edge`: Edge nodes
-- `cloud`: Cloud nodes
-- `sedge_Client`: node(s) acting as client/workload generator
-
-If you provisioned the infrastructure in EC2, you can also use
-`inventory.aws_ec2.yaml` that allows Ansible to
-dynamically retrieve information about our EC2 instances.
-Update `inventory.aws_ec2.yaml` (if needed) to set the regions where EC2
-instances have been launched.
-
-You can check that everything works by running:
-
-	ansible-inventory -i <inventory_file> --list
-
-
-### Deployment
-
-As Serverledge is currently under development, we deploy locally built binaries.
-To this end, you must provide the local path to Serverledge binaries 
-updating the `serverledge_local_bin_dir` variable (either modifying the relevant
-file or providing Ansible  "local vars").
-
-Run the playbook:
+Deploy Serverledge:
 
 	ansible-playbook [-i <inventory_file>] deploy_serverledge.yml
+	ansible-playbook [-i <inventory_file>] create_functions_and_workflows.yml
 
-Run a simple check to verify that the cluster is up:
+	ansible-playbook [-i <inventory_file>] profile_workflows.yml
 
-	ansible-playbook [-i <inventory_file>] check.yml
-
-### Benchmarking with JMeter
+### Benchmarking with Locust
 
 	ansible-playbook [-i <inventory_file>] run_experiment.yml
--- locust/locustfile.py
+++ locust/locustfile.py
@@ -15,10 +15,8 @@ locust.stats.PERCENTILES_TO_REPORT = [0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95]
 WORKFLOW_DATA = []
 
 class WorkflowData:
-    def __init__ (self, name, json_file, func_create_cmds, input_files):
+    def __init__ (self, name, input_files):
         self.name = name
-        self.json_file = json_file
-        self.func_create_cmds = func_create_cmds
         self.inputs = self.load_input_files(input_files)
         self.__next_input = 0
 
@@ -30,64 +28,14 @@ class WorkflowData:
                 inputs.append(jsonf.read())
         return inputs
 
-    def create(self, serverledge_cli, serverledge_host, serverledge_port):
-        base_cmd = f"{serverledge_cli} -H {serverledge_host} -P {serverledge_port} "
-
-        for create_cmd in self.func_create_cmds:
-            cmd = base_cmd + create_cmd 
-            output = subprocess.check_output(cmd, shell=True)
-            print(output)
-
-        cmd = base_cmd + f"create-workflow -s {self.json_file} -f {self.name}"
-        try:
-            output = subprocess.check_output(cmd, shell=True)
-            print(output)
-        except:
-            pass
-
     def get_next_input (self):
         f = self.inputs[self.__next_input]
         self.__next_input = (self.__next_input + 1) % len(self.inputs)
         return f
 
-#
-# WEATHER APP
-#
-cmds = []
-
-cmds.append("create -u -f weather --memory 500 --runtime custom --custom_image grussorusso/weatherfunc --input gemini_api_key:Text --input latitude:Float --input longitude:Float --output gemini_api_key:Text --output current_temperature:Float --output daily_rain_sum:ArrayFloat --output daily_max_temp:ArrayFloat --output daily_min_temp:ArrayFloat" )
-
-cmds.append("create -u -f adapter --memory 200 --runtime python310 --handler adapter.handler --src src/weather/adapter.py  --input gemini_api_key:Text --input current_temperature:Float --input daily_rain_sum:ArrayFloat --input daily_max_temp:ArrayFloat --input daily_min_temp:ArrayFloat --output prompt:Text --output gemini_api_key:Text")
-
-cmds.append("create -u -f gemini --memory 500 --runtime custom --custom_image grussorusso/geminifunc --input gemini_api_key:Text --input prompt:Text --output response:Text")
-
-WORKFLOW_DATA.append(WorkflowData("weatherApp", "src/weather/weather.json", cmds, ["input/weather1.json"]))
-
-#
-# SENTIMENT ANALYSIS APP
-#
-cmds = []
-
-cmds.append("create -u --function sa_retrieve --memory 256 --runtime custom --custom_image matnar/sa-retrieve --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input data_url:Text --input local_dir:Text --input object_name:Text --output status:Text  --output local_download:Bool --output uploaded:Bool --output object_name:Text")
-
-
-cmds.append("create -u --function sa_extract --memory 256 --runtime custom --custom_image matnar/sa-extract --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input tgz_input_object_name:Text --input subset:Float --input local_dataset_file:Text --input local_output_dir:Text --input output_train_object_name:Text --input output_test_object_name:Text --output status:Text --output train_object_name:Text --output test_object_name:Text")
-
-
-cmds.append("create -u --function sa_train --memory 1224 --runtime custom --custom_image matnar/sa-train --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input subset:Float --input max_features:Int --input train_object_data:Text --input local_train_file:Text --input local_model_file:Text --input local_vectorizer_file:Text --input output_model_object:Text --input output_vectorizer_object:Text --output status:Text --output model_object_name:Text --output vectorizer_object_name:Text")
-
-cmds.append("create -u --function sa_evaluate --memory 512 --runtime custom --custom_image matnar/sa-evaluate --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --input test_object_data:Text --input local_test_file:Text --input subset:Float --input local_model_file:Text --input local_vectorizer_file:Text --input input_model_object:Text --input input_vectorizer_object:Text --output status:Text --output accuracy:Float")
-
-WORKFLOW_DATA.append(WorkflowData("sentimentAnalysis", "src/sentiment/workflow.json", cmds, ["input/sentiment1.json"]))
-
-#
-# PERSON DETECTION APP
-#
-cmds = []
-cmds.append("create -u -f resize --memory 500 --runtime custom --custom_image grussorusso/resizefunc --input img:Text --output img:Text")
-cmds.append("create -u -f yoloFunc --memory 900 --runtime custom --custom_image grussorusso/yolofunc --input img:Text --output Img:Text --output Detections:ArrayText --output Count:Int")
-cmds.append("create -u -f cropFunc --memory 500 --runtime custom --custom_image grussorusso/cropfunc --input Img:Text --input Detections:ArrayText --input Count:Int --input minio_endpoint:Text --input minio_access_key:Text --input minio_secret_key:Text --output Objects:ArrayText")
-WORKFLOW_DATA.append(WorkflowData("personDetection", "src/person-detection/workflow.json", cmds, ["input/personDetection1.json"]))
+WORKFLOW_DATA.append(WorkflowData("weatherApp", ["input/weather1.json"]))
+WORKFLOW_DATA.append(WorkflowData("sentimentAnalysis", ["input/sentiment1.json"]))
+WORKFLOW_DATA.append(WorkflowData("personDetection", ["input/personDetection1.json"]))
 
 
 class ResponseLogger:
@@ -186,11 +134,6 @@ def on_locust_init(environment, **kwargs):
 
     serverledge_host = environment.host.replace("http://","")
     serverledge_host, serverledge_port = serverledge_host.split(":")
-
-    # Create workflows
-    print(f"Creating {len(WORKFLOW_DATA)} workflows...")
-    for workflow in WORKFLOW_DATA:
-        workflow.create("serverledge/serverledge-cli", serverledge_host, serverledge_port)
 
 @events.quitting.add_listener
 def _(environment, **kw):
